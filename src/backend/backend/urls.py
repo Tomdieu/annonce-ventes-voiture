d2a0ce75efdfae13550ef077d25f20fe6c02e247
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-06-29 23:47:22 +0200

    updated

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -84,3 +84,6 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+
+
+urlpatterns += staticfiles_urlpatterns()
