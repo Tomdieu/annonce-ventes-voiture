2230e20e7fd9065a1cf201d0bba67ff01b7891c6
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-07-13 23:56:22 +0200

    d

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: false,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    port: 3000, // you can replace this port with any port
-  }
+    host: true,
+    strictPort:true,
+    port: 3000,
+  },
 })
