9400a6f362a8203962df579a7442723815ee3710
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-08-25 14:42:13 +0200

    Update vite.config.ts

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: true, // Enable or disable minification
-    cssMinify: true, // Enable or disable CSS minification
-    chunkSizeWarningLimit: 1000, // Adjust the value according to your needs
-    // outDir: '../backend/frontend/build/',
-    assetsDir: 'static',
-  },
+  // build: {
+  //   minify: true, // Enable or disable minification
+  //   cssMinify: true, // Enable or disable CSS minification
+  //   chunkSizeWarningLimit: 1000, // Adjust the value according to your needs
+  //   // outDir: '../backend/frontend/build/',
+  //   assetsDir: 'static',
+  // },
   
 })
